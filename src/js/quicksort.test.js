184a09523c996a595a79c3f286e4abb35d1adfa1
Michael Sullivan <sullivanmichael344@gmail.com>
2024-10-05 21:36:00 -0400

    Edit readme

--- src/js/quicksort.test.js
+++ src/js/quicksort.test.js
@@ -1,5 +1,5 @@
 import { quicksort } from "./quicksort";
 
 test("sorts an array", () => {
-    expect(quicksort([7, 3, 6, 8, 9, 3])).toEqual([3, 3, 6, 7, 8, 9])
+    expect(quicksort([7, 3, 6, 4, 4, 8, 9, 3])).toEqual([3, 3, 4, 4, 6, 7, 8, 9])
 })
